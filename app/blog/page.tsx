0f61c988e369dcb6182aeb2be7d38c6e94206f89
kemeticapps <110548659+kemeticapps@users.noreply.github.com>
2023-11-17 06:07:38 +0200

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -20,8 +20,8 @@ export default async function ProjectsPage() {
     return acc;
   }, {} as Record<string, number>);
 
-  const top2 = allProjects.find((blog) => blog.slug === "planetfall")!;
-  const top3 = allProjects.find((blog) => blog.slug === "highstorm")!;
+  const top2 = allProjects.find((blog) => articale.slug === "planetfall")!;
+  const top3 = allProjects.find((blog) => articale.slug === "highstorm")!;
 
 
   return (
@@ -35,9 +35,9 @@ export default async function ProjectsPage() {
 <br><br>
 
            <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
-            {[top2, top3].map((project) => (
-              <Card key={project.slug}>
-                <Article project={project} views={views[blog.slug] ?? 0} />
+            {[top2, top3].map((articale) => (
+              <Card key={articale.slug}>
+                <Article articale={articale} views={views[articale.slug] ?? 0} />
               </Card>
             ))}
           </div>
