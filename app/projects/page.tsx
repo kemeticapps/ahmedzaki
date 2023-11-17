908c2d155351ca6e26757030fa9084fdc3fce8dc
kemeticapps <110548659+kemeticapps@users.noreply.github.com>
2023-11-17 05:31:01 +0200

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -41,15 +41,7 @@ export default async function ProjectsPage() {
               </Card>
             ))}
           </div>
-        </div>
-        <div className="hidden w-full h-px md:block bg-zinc-800" />
-
-        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
-          <div className="grid grid-cols-1 gap-4">
-           
-          </div>
-          <div className="grid grid-cols-1 gap-4">
-          </div>
+        
 
           
         </div>
