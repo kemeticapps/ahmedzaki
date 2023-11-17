5c6adaa309f10656a1bf20af84c3f15223b17c38
kemeticapps <110548659+kemeticapps@users.noreply.github.com>
2023-11-17 06:23:49 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          CEO & Founder at Kemetic | E-commerce Specialist
+          CEO & Founder at Kemetic | E-commerce Specialist | UI/UX Designer & Full-Stack Developer
         </h2>
       </div>
     </div>
