4a37b1e3999feb44241eee991ae8a40db06a3320
kemeticapps <110548659+kemeticapps@users.noreply.github.com>
2023-11-17 05:02:24 +0200

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,7 +20,6 @@ export default async function ProjectsPage() {
     return acc;
   }, {} as Record<string, number>);
 
-  const featured = allProjects.find((project) => project.slug === "unkey")!;
   const top2 = allProjects.find((project) => project.slug === "planetfall")!;
   const top3 = allProjects.find((project) => project.slug === "highstorm")!;
   const sorted = allProjects
