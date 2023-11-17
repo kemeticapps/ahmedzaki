fd047b36377581fbd8e2f09ff145e64ae3b18329
kemeticapps <110548659+kemeticapps@users.noreply.github.com>
2023-11-17 06:06:35 +0200

    Update header.tsx

--- app/blog/[slug]/header.tsx
+++ app/blog/[slug]/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
 type Props = {
-	project: {
+	articale: {
 		url?: string;
 		title: string;
 		description: string;
@@ -18,16 +18,16 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 	const [isIntersecting, setIntersecting] = useState(true);
 
 	const links: { label: string; href: string }[] = [];
-	if (project.repository) {
+	if (articale.repository) {
 		links.push({
 			label: "GitHub",
 			href: `https://github.com/${project.repository}`,
 		});
 	}
-	if (project.url) {
+	if (articale.url) {
 		links.push({
 			label: "Website",
-			href: project.url,
+			href: articale.url,
 		});
 	}
 	useEffect(() => {
@@ -88,7 +88,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 					</div>
 
 					<Link
-						href="/projects"
+						href="/blog"
 						className={`duration-200 hover:font-medium ${
 							isIntersecting
 								? " text-zinc-400 hover:text-zinc-100"
@@ -103,10 +103,10 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 				<div className="mx-auto max-w-7xl px-6 lg:px-8 text-center flex flex-col items-center">
 					<div className="mx-auto max-w-2xl lg:mx-0">
 						<h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl font-display">
-							{project.title}
+							{articale.title}
 						</h1>
 						<p className="mt-6 text-lg leading-8 text-zinc-300">
-							{project.description}
+							{articale.description}
 						</p>
 					</div>
 
