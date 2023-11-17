b68074dadf1a1aed7845398c0ceac2b6b5ea3c7c
kemeticapps <110548659+kemeticapps@users.noreply.github.com>
2023-11-17 04:25:05 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        AHMED ZAKI
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
@@ -37,12 +37,8 @@ export default function Home() {
         <h2 className="text-sm text-zinc-500 ">
           CEO & Founder at Kemetic | E-commerce Specialist{" "}
           <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+        
+          </Link>
         </h2>
       </div>
     </div>
