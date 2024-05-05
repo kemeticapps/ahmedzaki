60e40d97eadfbc820982f49e643dc6657da99175
kemeticapps <110548659+kemeticapps@users.noreply.github.com>
2024-05-06 02:31:59 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-sm flex duration-500 text-zinc-500 hover:text-zinc-300"
             >
               {item.name}
               <ArrowRight className="w-6 h-6 " />
@@ -35,7 +35,7 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-sm text-zinc-300 ">
           E-commerce Specialist | UI/UX Designer | Full-Stack Developer
         </h2>
       </div>
